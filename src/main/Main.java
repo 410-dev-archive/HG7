7379ff038b7735c5d547dcd8a7db669310d7c893
410 <58301450+410-dev@users.noreply.github.com>
2021-07-08 23:40:32 +0900

    Moved interpreters to independent class

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,7 +18,7 @@ import uiobjects.Screen;
 
 public class Main {
 	
-	private static String clientHost = "127.0.0.1";
+	public static String clientHost = "127.0.0.1";
 
 	public static void main(String[] args){
 		try {
@@ -63,33 +63,16 @@ public class Main {
 	}
 	
 	
-	public static String interpreter(String input) throws Exception {
-		if (input.equals("exit")) {
-			SocketIO.shouldClose = true;
-			return "SERVER:EXIT";
-		}else if (input.startsWith("start ")) {
-			input = input.replace("start ", "");
-			try {
-				start(input.split(" "));
-				return "SERVER:OK";
-			}catch(Exception e) {
-				Logger.writeExceptionLog(e, "interpreter.<start>");
-				return "SERVER:EXCEPTION:{" + Logger.convertStackTraceToString(e) + "}";
-			}
-		}else {
-			return "SERVER:INTERPRETER_FAILED";
-		}
-	}
-	
 	public static void start(String[] args) throws Exception {
 		
-		int screenWidth = Integer.parseInt(args[0]);
-		int screenHeight = Integer.parseInt(args[1]);
+		int screenWidth = 720;
+		int screenHeight = 480;
 		
 		if (args.length < 2) {
 			Logger.error("Minimum arguments are not given - Setting as 720x480");
-			screenWidth = 720;
-			screenHeight = 480;
+		}else {
+			screenWidth = Integer.parseInt(args[0]);
+			screenHeight = Integer.parseInt(args[1]);
 		}
 		
 		
@@ -98,47 +81,7 @@ public class Main {
 		Logger.info("Main screen height: " + screenHeight);
 		
 		WindowAllocator.createObjects(0, 0, screenWidth, screenHeight);
-		ViewDimension viewDimension = WindowAllocator.getScreenDimension();
-		
-		// Load main screen
-		GenericWindow background = new GenericWindow(0, 0, screenWidth, screenHeight, "WindowServer");
-		background.setBackground(Color.DARK_GRAY);
-		
-		// Load dock
-		Dock d = new Dock(viewDimension.X, viewDimension.Y, viewDimension.WIDTH, viewDimension.HEIGHT);
-		
-		// Show UI
-		WindowAllocator.addWindow(d.dock);
-		WindowAllocator.addWindow(background);
 		WindowAllocator.show();
-		
-		try {
-//			interpreter();
-		}catch(Exception e) {
-			e.printStackTrace();
-			Alert a = new Alert("Error", Logger.convertStackTraceToString(e));
-			WindowAllocator.addWindow(a.alert);
-		}
-	}
-	
-	
-	// Interprets the data responded from server after client sent signal
-	public static void serverResponseInterpreter(String data) {
-		
-	}
-	
-	// Interprets the data dispatched from RTServer
-	public static void serverSignalInterpreter(String data) {
-		try {
-			if (data.contains("Command: {exit}")) {
-				Logger.info("RTClient will stop client.");
-				SocketIO.clientMode("exit", clientHost);
-				System.exit(0);
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-			
-		}
 	}
 	
 
