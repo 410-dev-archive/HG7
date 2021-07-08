cf922a7001efdfda41a4d3a94ce6d8634a8b391e
410 <58301450+410-dev@users.noreply.github.com>
2021-07-08 23:41:08 +0900

    Create WindowData.java

---
+++ src/data/WindowData.java
@@ -0,0 +1,12 @@
+package data;
+
+import java.awt.Component;
+
+public class WindowData {
+	public String title;
+	public String content;
+	public Component window;
+	public String[] buttonIds;
+	public String[] buttonActions;
+	public String[] buttonTexts;
+}
