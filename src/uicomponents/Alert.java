b7ff531acdc9effb1d3e8a8a8c26b0a7c65410a5
410 <58301450+410-dev@users.noreply.github.com>
2021-07-09 05:15:16 +0900

    Programmatically generate alert - Need to add layout

--- src/uicomponents/Alert.java
+++ src/uicomponents/Alert.java
@@ -1,10 +1,17 @@
 package uicomponents;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import IO.SocketIO;
+import data.AlertData;
+import data.ButtonData;
 import data.ViewDimension;
 import main.WindowAllocator;
 import main.WindowPositioner;
@@ -12,19 +19,33 @@ import uiobjects.GenericWindow;
 
 public class Alert {
 	public JPanel alert;
+	public String id;
 	
-	public Alert(String title, String data) {
+	public Alert(AlertData data) {
 		ViewDimension alertDimension = new ViewDimension();
-		alertDimension.HEIGHT = 70;
-		alertDimension.WIDTH = 400;
+		alertDimension.HEIGHT = data.height;
+		alertDimension.WIDTH = data.width;
 		alertDimension = WindowPositioner.getNewWindowPositionOfCenter(WindowAllocator.getScreenDimension(), alertDimension);
-		alert = new GenericWindow(alertDimension.X, alertDimension.Y, alertDimension.WIDTH, alertDimension.HEIGHT, title);
+		alert = new GenericWindow(alertDimension.X, alertDimension.Y, alertDimension.WIDTH, alertDimension.HEIGHT, data.title);
+		id = data.parentID;
 		
 		JLabel content = new JLabel();
-		content.setText(data);
+		content.setText(data.text);
 		
 		JLabel titleText = new JLabel();
-		titleText.setText(title);
+		titleText.setText(data.title);
+		
+		
+		for(ButtonData buttonData : data.buttons) {
+			JButton button = new JButton();
+			button.setText(buttonData.text);
+			button.addActionListener(new ActionListener() {
+			    @Override
+			    public void actionPerformed(ActionEvent e) {
+			        SocketIO.sendMessageToClient("ButtonEvent_" + data.parentID + ":" + buttonData.onClickCommand);
+			    }
+			});
+		}
 		
 		alert.add(content);
 		alert.add(titleText);
