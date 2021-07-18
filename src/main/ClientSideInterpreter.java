c03dedce2c5b772728b73cd3b12b9619785e48be
410 <58301450+410-dev@users.noreply.github.com>
2021-07-19 02:47:28 +0900

    Changed shell execution

--- src/main/ClientSideInterpreter.java
+++ src/main/ClientSideInterpreter.java
@@ -23,47 +23,49 @@ public class ClientSideInterpreter {
 	        JSONObject parsed = (JSONObject) parser.parse(data);
 			
 			// JSON 으로부터 Command 키의 값을 가져옴
-	        String command = parsed.get("Command").toString();
-	        
-	        // 명령 처리
-			if (command.equals("exit")) {
-				Logger.info("RTClient will stop client.");
-				Logger.info("RTClient will send stop signal to server.");
-
-				// 서버로 종료 명령 전송
-				SocketIO.clientMode(command, Main.clientHost);
-				System.exit(0);
-			
-			// 셸 명령어를 처리하는 명령
-			}else if (command.startsWith("<exec> ")) {
+	        String cmd = parsed.get("Command").toString();
+	        String[] commands = cmd.split("<next>");
+		    
+	        for(String command : commands) {
+		        // 명령 처리
+				if (command.equals("exit")) {
+					Logger.info("RTClient will stop client.");
+					Logger.info("RTClient will send stop signal to server.");
+	
+					// 서버로 종료 명령 전송
+					SocketIO.clientMode(command, Main.clientHost);
+					System.exit(0);
 				
-				// 명령어 분리
-				command = command.substring("<exec> ".length());
-				Logger.info("RTClient will execute shell script: " + command);
-				
-				// 셸 명령 실행
-				Runtime run = Runtime.getRuntime();
-				Process pr = run.exec(command);
-				pr.waitFor();
-				
-				// 명령 실행 이후 출력값을 받아옴
-				BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-				String line = "";
-				String tmpLine = "";
-				while ((tmpLine=buf.readLine())!=null) {
-					line += tmpLine + "<linebreak>";
+				// 셸 명령어를 처리하는 명령
+				}else if (command.startsWith("<exec> ")) {
+					
+					// 명령어 분리
+					command = command.substring("<exec> ".length());
+					Logger.info("RTClient will execute shell script: " + command);
+					
+					// 셸 명령 실행
+					Runtime run = Runtime.getRuntime();
+					Process pr = run.exec(command);
+					pr.waitFor();
+					
+					// 명령 실행 이후 출력값을 받아옴
+					BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+					String line = "";
+					String tmpLine = "";
+					while ((tmpLine=buf.readLine())!=null) {
+						line += tmpLine + "<linebreak>";
+					}
+					
+					Logger.info("Shell returned: " + line);
+					
+					// 서버로 전송
+					SocketIO.clientMode(line, Main.clientHost);
+				}else { // 서버 요청을 이해하지 못했을 때 수신된 명령을 다시 서버로 반환 (프론트엔드 처리 명령일 가능성이 큼)
+					Logger.error("Server requested unknown command. Sending command back to server.");
+					SocketIO.clientMode(command.replace("<inside_next>", "<next>"), Main.clientHost); // 서버로 전송
 				}
-				
-				Logger.info("Shell returned: " + line);
-				
-				// 서버로 전송
-				SocketIO.clientMode(line, Main.clientHost);
-			}else { // 서버 요청을 이해하지 못했을 때 수신된 명령을 다시 서버로 반환 (프론트엔드 처리 명령일 가능성이 큼)
-				Logger.error("Server requested unknown command. Sending command back to server.");
-				SocketIO.clientMode(command, Main.clientHost); // 서버로 전송
-			}
-			
-			Logger.info("Interpretation OK");
+				Logger.info("Interpretation OK for line: " + command);
+	        }
 		}catch(Exception e) {
 			e.printStackTrace();
 			Logger.error(Logger.convertStackTraceToString(e));
